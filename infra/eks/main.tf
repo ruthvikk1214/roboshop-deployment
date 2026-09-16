54b33a26e8853eb8156ab7408de5137a70b43cb6
Ruthvekh Kadudhuri <104150519+ruthvikk1214@users.noreply.github.com>
2026-09-16 21:27:10 +0000

    fix(eks): add k8s provider and resolve addon conflicts

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -15,7 +15,18 @@ terraform {
 # Data source – current region (used for the EKS module)
 # ------------------------------------------------------------------
 data "aws_region" "current" {}
-
+# ------------------------------------------------------------------
+# Authenticate Terraform to the EKS Cluster
+# ------------------------------------------------------------------
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
 # ------------------------------------------------------------------
 # EKS Cluster – using the official Terraform AWS EKS module
 # ------------------------------------------------------------------
